2c99d322336a18f1e54d7161833d6981fb254717
Golovii Sergii <golovii.sergii@gmail.com>
2015-06-23 20:45:49 +0300

    Refactor

--- src/com/main/Game.java
+++ src/com/main/Game.java
@@ -12,27 +12,35 @@ import javax.swing.JPanel;
 
 public class Game extends JPanel {
 
-	private Dimension gameWindow;
+	private Dimension gameFrame;
 	private Player player;
 	private Ball ball;
-	private volatile boolean isRunning = false;
-	private volatile boolean isPaused = false;
-	private int border = 100;
+	private volatile boolean isRunning;
+	private volatile boolean isPaused;
+	private int border;
+	private int playerSpeed;
 
-	Game(JFrame windowFrame) {
-		super.setSize(windowFrame.getSize());
+	Game(JFrame mainFrame) {
+		super.setSize(mainFrame.getSize());
+		
+		isRunning = false;
+		isPaused = false;
+		playerSpeed = 25;
+		border = 100;
+		//Instead of popular Linux DE (such as Unity or KDE), windows in Microsoft Windows OS family has borders, 
+		//that closes elements of the game, so I decided to use "frame inside frame" (we need to go deeper...)
+		//with the border, which controlling by a "border" variable.
+		gameFrame = new Dimension(mainFrame.getWidth() - border, mainFrame.getHeight() - border);
 
-		gameWindow = new Dimension(windowFrame.getWidth() - border, windowFrame.getHeight() - border);
-
-		int tmpWidth = (gameWindow.width - Player.WIDTH) / 2;
-		int tmpHeight = (gameWindow.height - Player.HEIGHT) / 2;
+		int tmpWidth = (gameFrame.width - Player.WIDTH) / 2;
+		int tmpHeight = (gameFrame.height - Player.HEIGHT) / 2;
 
 		player = new Player(this, tmpWidth, tmpHeight);
-		player.setPositionY(gameWindow.height - Player.HEIGHT);
+		player.setPositionY(gameFrame.height - Player.HEIGHT);
 
 		ball = new Ball(this);
 		
-		startGame(windowFrame);
+		startGame(mainFrame);
 	}
 	
 	private void startGame(JFrame frame) {
@@ -43,9 +51,9 @@ public class Game extends JPanel {
 					if (event.getKeyCode() == event.VK_ENTER) start();	
 				} else {
 					if (event.getKeyCode() == event.VK_RIGHT)
-						player.move(25);
+						player.move(playerSpeed);
 					if (event.getKeyCode() == event.VK_LEFT)
-						player.move(-25);
+						player.move(-playerSpeed);
 				}
 			}
 		});
@@ -73,17 +81,18 @@ public class Game extends JPanel {
 	@Override
 	public void paint(Graphics graphics) {
 		super.paint(graphics);
-		graphics.translate((this.getWidth() - gameWindow.width) / 2, (this.getHeight() - gameWindow.height) / 2);
+		//placing gameFrame in the middle of mainFrame
+		graphics.translate((this.getWidth() - gameFrame.width) / 2, (this.getHeight() - gameFrame.height) / 2);
 		graphics.setColor(new Color(31, 0, 15));
-		graphics.fillRect(0, 0, gameWindow.width, gameWindow.height);
+		graphics.fillRect(0, 0, gameFrame.width, gameFrame.height);
 		player.render(graphics);
 		ball.render(graphics);
 	}
 
 	public void fail() {
 		pause();
-		ball.setPosition(gameWindow.width / 2, gameWindow.height / 2);
-		player.setPositionX((gameWindow.width - player.WIDTH) / 2);
+		ball.setPosition(gameFrame.width / 2, gameFrame.height / 2);
+		player.setPositionX((gameFrame.width - player.WIDTH) / 2);
 		repaint();
 	}
 
@@ -97,11 +106,11 @@ public class Game extends JPanel {
 	}
 
 	public Dimension getGameWindow() {
-		return gameWindow;
+		return gameFrame;
 	}
 
 	public void setGameWindow(Dimension gameWindow) {
-		this.gameWindow = gameWindow;
+		this.gameFrame = gameWindow;
 	}
 
 	public Player getPlayer() {
