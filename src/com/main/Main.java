4dfd1db703e4cbdea2500b2a755290e0b93ea197
Golovii Sergii <golovii.sergii@gmail.com>
2015-08-26 09:04:15 +0300

    test commit

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		JFrame frame = new JFrame();
 		frame.setSize(900, 700);
 		frame.setTitle("Simple Arkanoid");
